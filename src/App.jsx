cf98a126296bcd5ef918531da758e8429906f584
pratiksha <pratu@gmail.com>
2024-10-01 17:16:38 +0530

    NGO website modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBluetoothB, FaInstagram } from 'react-icons/fa'
 import { FaTwitter } from 'react-icons/fa'
 import { FaWhatsapp } from 'react-icons/fa'
@@ -10,8 +10,14 @@ import { FaLightbulb } from 'react-icons/fa'
 import { FaEmpire } from 'react-icons/fa'
 import photo1 from './assets/photo1.jpg'
 import { FaBars } from 'react-icons/fa'
+
 // import Contact from './Contact.jsx'
 function App() {
+  const [isMenuOpen,setIsMenuOpen]=useState(false);
+  const toggleMenu=()=>
+  {
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
     <div>
       <div className="navbar">
@@ -20,25 +26,31 @@ function App() {
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Blog</a></li>
-          <li><a href="Contact.jsx">Contact</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
-        <div className="bars">
+        <div className="bars" onClick={toggleMenu}>
           <FaBars/>
         </div>
       </div>
+       <div className={`hamburger-list ${isMenuOpen?'show':''}`}>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
+        <a href="#">Contact</a>
+      </div> 
       <div className="main-section">
-        {/* <img src={photo1} alt="" /> */}
-        <h1>Spread Happiness Everywhere.</h1>
+            <h1>Spread Happiness Everywhere.</h1>
       </div>
-      <div className="info-section">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, corrupti est, quos, vitae at dolore ipsum perferendis provident repellat ipsa voluptates animi dicta minima? Nisi nobis illum iste? Enim cumque mollitia, deleniti numquam exercitationem perferendis! Quos quasi hic ipsa sint omnis explicabo inventore voluptates non accusantium! Tempora exercitationem possimus, veritatis cum rem ipsa nisi, maxime officia, ipsam quo porro vel. Corrupti reprehenderit ullam est ab dolore, distinctio explicabo eum culpa quaerat nam. Dolorem officia aspernatur perspiciatis tenetur quidem, dolores natus?<br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, culpa. Quos nihil, maxime quia eaque quaerat natus animi inventore distinctio soluta tempora exercitationem fugiat cum consequuntur fuga autem? Accusamus minima quia aut impedit! Obcaecati nobis nostrum voluptatibus iste nam, quam fugit sed. Labore atque consectetur magnam suscipit? Nemo, blanditiis quaerat.
-        </p>
+     <div className="info-wrapper">
+     <div className="info-section">
+        <p>At Live Life Foundation, we are dedicated to creating a brighter future for underprivileged communities through education, empowerment, and sustainable development. Our mission is to uplift lives by providing access to quality education, promoting better livelihood opportunities, and fostering social awareness. We believe that every individual has the potential to make a difference, and through our various programs, we strive to unlock that potential. From educational initiatives to health and wellness campaigns, we work tirelessly to bridge the gap between privilege and poverty. By fostering a spirit of compassion and collective responsibility, we aim to create a world where no one is left behind. Join us on this journey to spread happiness, build resilient communities, and empower individuals to lead a dignified and fulfilling life. Together, we can create a ripple effect of positive change that transforms lives for generations to come.</p>
       </div>
+     </div>
       <div className="social-section">
         <div className="social-icon">
           <div className="icons">
             <FaSchool/>
+            
             <h6>Education & learning</h6>
           </div>
           <div className="icons">
@@ -62,7 +74,7 @@ function App() {
       <div className="cycle"></div>
       <div className="contact-heading">
           <h1>Get in Touch</h1>
-        </div>
+      </div>
       <div className="contact-section">
         <div className="contact-wrapper">
           <div className="leftcontact">
@@ -97,7 +109,7 @@ function App() {
       </div>
       <footer className="footer">
         <div className="toppart">
-          <p>Don't miss anything,To know about our new products visit <br />out below social media plateforms.</p>
+        <p>Don't miss anything,To know about our new products visit <br />out below social media plateforms.</p>
           <ul className="Social_media">
             <li><FaInstagram/></li>
             <li><FaTwitter/></li>
